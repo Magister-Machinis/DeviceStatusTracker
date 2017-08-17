ace9ac1df6be900a7b1f39a61334e28e26e26c5b
8942 <8942@DESKTOP-B0J60L5>
2017-08-17 12:00:10 -0400

    tweak

--- policy_automation.py
+++ policy_automation.py
@@ -6,7 +6,7 @@ from builtins import input
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
-from email.Utils import formatdate
+from email.utils import formatdate
 from email import encoders
 import sys
 import itertools
