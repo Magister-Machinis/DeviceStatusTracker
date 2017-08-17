7938f42c5d5f8a40a19ebc9951437534535b66ba
8942 <8942@DESKTOP-B0J60L5>
2017-08-17 11:39:34 -0400

    bugfix

--- policy_automation.py
+++ policy_automation.py
@@ -6,6 +6,7 @@ from builtins import input
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
+from email.Utils import formatdate
 from email import encoders
 import sys
 import itertools
